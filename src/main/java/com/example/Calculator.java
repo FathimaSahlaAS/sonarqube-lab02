4a407b6db61a813a44a52c820075e6e93637cd0a
FathimaSahlaAS <asfathimasahla@gmail.com>
2026-01-29 21:22:12 +0530

    Fix quality issues

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,34 +2,22 @@ package main.java.com.example;
 
 public class Calculator {
 
-    // EVEN WORSE: longer, more complex, duplicated logic
-    public int calculate(int a, int b, String op) {
-        if (op.equals("add")) {
-            return a + b;
-        } else if (op.equals("add-again")) {
-            return a + b; // DUPLICATION
-        } else if (op.equals("sub")) {
-            return a - b;
-        } else if (op.equals("sub-again")) {
-            return a - b; // DUPLICATION
-        } else if (op.equals("mul")) {
-            return a * b;
-        } else if (op.equals("div")) {
-            if (b == 0) {
+    public int calculate(int a, int b, String operation) {
+        switch (operation) {
+            case "add":
+                return a + b;
+            case "sub":
+                return a - b;
+            case "mul":
+                return a * b;
+            case "div":
+                return (b != 0) ? a / b : 0;
+            case "mod":
+                return a % b;
+            case "pow":
+                return (int) Math.pow(a, b);
+            default:
                 return 0;
-            } else {
-                return a / b;
-            }
-        } else if (op.equals("mod")) {
-            return a % b;
-        } else if (op.equals("pow")) {
-            int result = 1;
-            for (int i = 0; i < b; i++) {
-                result = result * a;
-            }
-            return result;
-        } else {
-            return 0;
         }
     }
 
@@ -42,8 +30,4 @@ public class Calculator {
         return a + b;
     }
 
-    // INTENTIONAL DUPLICATION
-    public int addAgain(int a, int b) {
-        return a + b;
-    }
 }
--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -2,6 +2,7 @@ package main.java.com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class UserService {
@@ -27,11 +28,13 @@ public class UserService {
         System.out.println("I am never called");
     }
 
-    // EVEN WORSE: another SQL injection
     public void deleteUser(String username) throws Exception {
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
-        Statement st = conn.createStatement();
-        String query = "DELETE FROM users WHERE name = '" + username + "'";
-        st.execute(query);
+        String query = "DELETE FROM users WHERE name = ?";
+        PreparedStatement pst = conn.prepareStatement(query);
+        pst.setString(1, username);
+        pst.execute();
+        pst.close();
+        conn.close();
     }
 }
